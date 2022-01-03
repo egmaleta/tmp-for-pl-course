89c660f8ae5953c41d38ed7ca398d9ae22b8738a
Eduardo García Maleta <yeyon.personal@protonmail.com>
2022-01-03 18:15:38 -0500

    Implemented Fib using constexpr

---
+++ constexpr_fib.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#ifndef NUMBER
+#define NUMBER 20
+#endif
+
+using namespace std;
+
+
+constexpr long int fib(int n){
+    return (n==1 || n==2) ? n : fib(n-1) + fib(n-2);
+}
+
+
+int main(){
+    constexpr int fib1 = fib(NUMBER); // se obtiene an tiempo de compilacion
+    cout << fib1 << '\n';
+
+    int fib2 = fib(NUMBER); // este no
+    cout << fib2 << '\n';
+}
